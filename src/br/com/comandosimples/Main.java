6663041ebedc1de414b473ceb0029bf68ca79116
Lucas Santos <lucas.prog@outlook.com>
2022-03-19 17:26:03 -0300

    Escondendo-Working-Directory

--- src/br/com/comandosimples/Main.java
+++ src/br/com/comandosimples/Main.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-      //  System.out.println("Working Directory: " + System.getProperty("user.dir"));
+        
 
         File file = new File("src/br/com/comandosimples/arquivo.txt");
         InputStream is = new FileInputStream(file);
