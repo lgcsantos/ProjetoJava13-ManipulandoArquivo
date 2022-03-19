18a78a1eef3525d9dea4d25bbfe2179f957e4ee8
Lucas Santos <lucas.prog@outlook.com>
2022-03-19 17:17:40 -0300

    Initial-commit_Como-Ler-Arquivo.txt

---
+++ src/br/com/comandosimples/Main.java
@@ -0,0 +1,22 @@
+package br.com.comandosimples;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Working Directory: " + System.getProperty("user.dir"));
+
+        File file = new File("src/br/com/comandosimples/arquivo.txt");
+        InputStream is = new FileInputStream(file);
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+
+        String line = br.readLine();
+
+        while (line != null){
+            System.out.println(line);
+            line = br.readLine();
+        }
+    }
+}
